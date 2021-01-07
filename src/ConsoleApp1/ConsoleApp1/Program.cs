95b348193fc22d240cb3f0cd909d7974caf3bd76
Will Allan <will.allan@disneystreaming.com>
2021-01-07 15:11:48 -0800

    Odds calculation

--- src/ConsoleApp1/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
 
 namespace ConsoleApp1
 {
@@ -10,7 +7,33 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Enter number of balls: ");
+            var r = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter max ball number: ");
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter max powerball number: ");
+            var p = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Odds are 1 in " + CalculateOdds(r, n, p));
+        }
+
+        private static BigInteger CalculateFactorial(int n)
+        {
+            BigInteger f = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                f = f * i;
+            }
+
+            return f;
+        }
+
+        private static BigInteger CalculateOdds(int r, int n, int p)
+        {
+            return (CalculateFactorial(n) / (CalculateFactorial(r) * CalculateFactorial(n - r))) * p;
         }
     }
 }
